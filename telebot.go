3a7191604d623b917725bcb117a79c2bc087fbf5
zhaojiajin <zhaojiajin@gmail.com>
2017-02-13 21:59:11 +0800

    getme

--- telebot.go
+++ telebot.go
@@ -1,8 +1,37 @@
 package telebot
 
-import "net/http"
+import (
+    "net/http"
+    "io/ioutil"
+    "errors"
+    "fmt"
+)
 
-func Get(url string)(resp *http.Response){
-    resp, _ = http.Get(url)
-    return resp
+
+func checkErr(err error){
+    if err != nil{
+        panic(err)
+    }
+}
+
+
+type Bot struct{
+    Api string
+    Key string
+}
+
+
+func (b Bot)Getme()(string, error){
+    url := fmt.Sprintf("%v/bot%v/getMe",b.Api,b.Key)
+    resp, err := http.Get(url)
+    if err != nil{
+        return "", errors.New("get request fail," + err.Error())
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil{
+        return "", errors.New("read get request data fail," + err.Error())
+    }
+    s := string(body)
+    return s,nil
 }
