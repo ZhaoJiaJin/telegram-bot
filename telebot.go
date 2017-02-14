915ff028736a141fc321be44cc3a54135947e093
赵家进 <zhaojiajin@zhaojiajinMacBook-Air.local>
2017-02-14 13:45:36 +0800

    加入消息结构体

--- telebot.go
+++ telebot.go
@@ -20,8 +20,25 @@ type Bot struct{
     Api string
     Key string
     Maxid float64
+    mess chan string
 }
 
+type Mess struct{
+    Update_id float64
+    Mess_id float64
+    Chat interface{}
+    Date float64
+    Text string
+    Entity interface{}
+}
+
+
+func (b *Bot)GetChan(){
+    if b.mess == nil{
+        b.mess = make(chan string)
+    }
+
+}
 
 func (b *Bot)Getupdate()(string,error){
     url := fmt.Sprintf("%v/bot%v/getUpdates",b.Api,b.Key)
@@ -42,7 +59,9 @@ func (b *Bot)Getupdate()(string,error){
         dic_mess := v.(map[string]interface{})
         up_id := dic_mess["update_id"].(float64)
         if up_id > b.Maxid{
-            fmt.Println(dic_mess)
+            mess_con := dic_mess["message"].(map[string]interface{})
+            one_mess := Mess{Update_id:up_id,Mess_id:mess_con["message_id"].(float64),Chat:mess_con["chat"],Date:mess_con["date"].(float64),Text:mess_con["text"].(string),Entity:mess_con["entities"]}
+            fmt.Println(one_mess)
             b.Maxid = up_id
         }
     }
