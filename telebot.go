bf5197a2e2fb4605514ebe01c02e6d150c9b91b5
赵家进 <zhaojiajin@zhaojiajinMacBook-Air.local>
2017-02-14 19:17:30 +0800

    加入消息channel

--- telebot.go
+++ telebot.go
@@ -4,6 +4,7 @@ import (
     "net/http"
     "io/ioutil"
     "errors"
+    "time"
     "encoding/json"
     "fmt"
 )
@@ -20,7 +21,8 @@ type Bot struct{
     Api string
     Key string
     Maxid float64
-    mess chan string
+    Mess_chan chan Mess
+    receive bool
 }
 
 type Mess struct{
@@ -32,27 +34,39 @@ type Mess struct{
     Entity interface{}
 }
 
+func (b *Bot)Stop_receive(){
+    b.receive = false
+}
 
 func (b *Bot)GetChan(){
-    if b.mess == nil{
-        b.mess = make(chan string)
+    b.receive = true
+    go b.Receive()
+}
+
+func (b *Bot)Receive(){
+    for ;b.receive; {
+        b.Getupdate()
+        time.Sleep(2000 * time.Millisecond)
     }
 
 }
 
-func (b *Bot)Getupdate()(string,error){
+func (b *Bot)Getupdate(){
+    if b.Mess_chan == nil{
+        b.Mess_chan = make(chan Mess,1000)
+    }
     url := fmt.Sprintf("%v/bot%v/getUpdates",b.Api,b.Key)
     s,err := send(url)
     if err != nil{
-        return "",err
+        fmt.Println(err)
     }
     res := make(map[string]interface{})
     err = json.Unmarshal([]byte(s), &res)
     if err != nil{
-        return "",err
+        fmt.Println(err)
     }
     if res["ok"] != true{
-        return "",errors.New("resp err; " + s)
+        fmt.Println("resp err:"+s)
     }
 
     for _,v := range(res["result"].([]interface{})){
@@ -61,11 +75,12 @@ func (b *Bot)Getupdate()(string,error){
         if up_id > b.Maxid{
             mess_con := dic_mess["message"].(map[string]interface{})
             one_mess := Mess{Update_id:up_id,Mess_id:mess_con["message_id"].(float64),Chat:mess_con["chat"],Date:mess_con["date"].(float64),Text:mess_con["text"].(string),Entity:mess_con["entities"]}
-            fmt.Println(one_mess)
+            //fmt.Println(one_mess)
+            fmt.Println("put message",one_mess)
+            b.Mess_chan <- one_mess
             b.Maxid = up_id
         }
     }
-    return s,nil
 }
 
 func (b Bot)Getme()(string, error){
